fccfa2bf808ecaec30a257c9dafef0463bb5294d
nishuoshenme <arraytoobig@gmail.com>
2020-04-25 13:00:49 +0800

    done

--- jni/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE     := libriru_fgofpsunlocker
-LOCAL_LDLIBS += -ldl -llog
-LOCAL_LDFLAGS := -Wl
-LOCAL_SRC_FILES:= main.c riru.c fake_dlfcn.c
-include $(BUILD_SHARED_LIBRARY)
--- jni/Application.mk
+++
@@ -1,2 +0,0 @@
-APP_ABI := arm64-v8a#armeabi-v7a
-APP_PLATFORM := android-23
